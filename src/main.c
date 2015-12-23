a0ac31136c3639aaf82c3d8487fb6f4122c225c4
Nick Teo <gt224@cornell.edu>
2015-12-23 17:49:06 -0500

    Send transcription to the phone

--- src/main.c
+++ src/main.c
@@ -1,11 +1,28 @@
 #include <pebble.h>
 
+#define KEY_PHONE_NUMBER 0
+#define KEY_MESSAGE 1
+
+char* initialString = "Speak your mind!";
+  
 static DictationSession *s_dictation_session;
 static Window *s_main_window;
 static TextLayer *s_output_layer;
 
 // Declare a buffer for the DictationSession
 static char s_last_text[512];
+
+
+
+// Send a message
+static void sendString(int key, char* value) {
+  DictionaryIterator *iter;
+  app_message_outbox_begin(&iter);
+
+  dict_write_cstring(iter, key, value);
+
+  app_message_outbox_send();
+}
 
 // Dictation Callback
 static void dictation_session_callback(DictationSession *session, DictationSessionStatus status, 
@@ -14,6 +31,9 @@ static void dictation_session_callback(DictationSession *session, DictationSessi
     // Display the dictated text
     snprintf(s_last_text, sizeof(s_last_text), "Transcription:\n\n%s", transcription);
     text_layer_set_text(s_output_layer, s_last_text);
+    
+    // Send the dictated text to the phone
+    sendString(KEY_MESSAGE, transcription);
   } else {
     // Display the reason for any error
     static char s_failed_buff[128];
@@ -27,14 +47,27 @@ static void dictation_session_callback(DictationSession *session, DictationSessi
 // App Message Handlers //////////////////////////////////////////////////////////////////
 //////////////////////////////////////////////////////////////////////////////////////////
 
-static void outbox_sent_handler(DictionaryIterator *iter, void *context) {
+static void outbox_sent_callback(DictionaryIterator *iter, void *context) {
   // Ready for next command
-  text_layer_set_text(s_output_layer, "Press up or down.");
+  text_layer_set_text(s_output_layer, initialString);
 }
 
-static void outbox_failed_handler(DictionaryIterator *iter, AppMessageResult reason, void *context) {
+static void outbox_failed_callback(DictionaryIterator *iter, AppMessageResult reason, void *context) {
   text_layer_set_text(s_output_layer, "Send failed!");
   APP_LOG(APP_LOG_LEVEL_ERROR, "Fail reason: %d", (int)reason);
+}
+
+static void inbox_received_callback(DictionaryIterator *iterator, void *context) {
+  // Get the first pair
+  /*Tuple *data = dict_find(iterator, KEY_DATA);
+  if (data) {
+    snprintf(s_buffer, sizeof(s_buffer), "Received '%s'", data->value->cstring);
+    text_layer_set_text(s_output_layer, s_buffer);
+  }*/
+}
+
+static void inbox_dropped_callback(AppMessageResult reason, void *context) {
+  APP_LOG(APP_LOG_LEVEL_ERROR, "Message dropped!");
 }
 
 //////////////////////////////////////////////////////////////////////////////////////////
@@ -49,7 +82,7 @@ static void main_window_load(Window *window) {
   const GEdgeInsets text_insets = GEdgeInsets((bounds.size.h - text_height) / 2, 0);
 
   s_output_layer = text_layer_create(bounds);
-  text_layer_set_text(s_output_layer, "Speak your mind!");
+  text_layer_set_text(s_output_layer, initialString);
   text_layer_set_text_alignment(s_output_layer, GTextAlignmentCenter);
   text_layer_set_overflow_mode(s_output_layer, GTextOverflowModeWordWrap);
   layer_add_child(window_layer, text_layer_get_layer(s_output_layer));
@@ -93,8 +126,8 @@ static void init() {
   // Open AppMessage
   app_message_register_inbox_received(inbox_received_callback);
   app_message_register_inbox_dropped(inbox_dropped_callback);
-  app_message_register_outbox_sent(outbox_sent_handler);
-  app_message_register_outbox_failed(outbox_failed_handler);
+  app_message_register_outbox_sent(outbox_sent_callback);
+  app_message_register_outbox_failed(outbox_failed_callback);
   app_message_open(app_message_inbox_size_maximum(), app_message_outbox_size_maximum());
 }
 
